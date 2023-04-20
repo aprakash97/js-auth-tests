e298b413aee76df4b0b0959ef5adc1876be6e8a1
aprakash97 <prakashakrakr@gmail.com>
2023-04-20 12:19:40 +0530

    react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
