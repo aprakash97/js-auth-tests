952ca2f259726b4eb2e4061596a8f40bf8d7bcd1
aprakash97 <prakashakrakr@gmail.com>
2023-05-05 20:14:30 +0530

    all unwanted files deleted

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,7 +1,7 @@
 export const initialItems = [
   { userID: 1, name: "abc", email: "abc@gmail.com" },
   { userID: 2, name: "tester", email: "tester@gmail.com" },
-  { userID: 3, name: "customObj", email: "customObj@gmail.com" }, //email, quality didn't changed
+  { userID: 3, name: "customObj", email: "customObj@gmail.com" },
   { userID: 4, name: "finalTesting", email: "finally@gmail.com" },
   { userID: 5, name: "jsxBoy", email: "jxxxx@gmail.com" },
   { userID: 6, name: "working", email: "work@gmail.com" },
