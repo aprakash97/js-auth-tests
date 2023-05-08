00a56b96c4b23a3fe5d35e44e8da5d254268475d
aprakash97 <prakashakrakr@gmail.com>
2023-05-08 09:19:56 +0530

    working git before major change

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -3,8 +3,8 @@ import User from "./User";
 export const UserList = ({ items }) => {
   return (
     <div className="main">
-      {items.map((item) => (
-        <User {...item} key={item.userID} />
+      {items.map((user) => (
+        <User {...user} key={user.userID} />
       ))}
     </div>
   );
